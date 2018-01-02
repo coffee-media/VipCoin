0a9c1d30c5bc61638c3c52076527a7f651c00a02
SirAricost <coffee-media@users.noreply.github.com>
2018-01-02 17:18:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 if (localStorage.coins) {
     localStorage.coins = Number(localStorage.coins);
-    document.getElementById("CURRENT BALANCE").innerHTML = 
-        "<strong>CURRENT BALANCE:</strong>" + 
-        localStorage.coins;
     
 } else {
     localStorage.coins = 50;
-    document.getElementById("CURRENT BALANCE").innerHTML = 
-        "<strong>CURRENT BALANCE:</strong>" + 
-        localStorage.coins;
 }
 
