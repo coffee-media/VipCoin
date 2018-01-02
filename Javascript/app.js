106b10386a7a4762480cfd933f17a711896013f3
SirAricost <coffee-media@users.noreply.github.com>
2018-01-02 17:09:20 -0500

    Create app.js

---
+++ Javascript/app.js
@@ -0,0 +1,6 @@
+if (localStorage.coins) {
+    localStorage.coins = Number(localStorage.coins);
+} else {
+    localStorage.coins = 50;
+}
+document.getElementById("CURRENT BALANCE").innerHTML = "<strong>CURRENT BALANCE:</strong>" + localStorage.coins;
